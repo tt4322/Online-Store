7de99467da6bb2f56a4f33e2c0a82e94fa58f181
SantiagoAl <44738682+SantiagoAl@users.noreply.github.com>
2020-04-14 15:31:23 -0400

    php file that inserts a product into the database

---
+++ api/add.php
@@ -0,0 +1,32 @@
+<?php
+    $yourURL = "./addRemove.php";
+    $dbhost = "us-cdbr-iron-east-01.cleardb.net";
+    $dbname = "heroku_e78a82b925de5db";
+    $username = "b56c5bf006c12b";
+    $password = "5e05a4dd";
+
+    try {
+        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
+
+        $discount_percentage = 0;
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $name = $_POST['product_name'];
+
+        $stmt = $dbh->prepare("INSERT INTO products (name, description, price, discount_percentage) VALUES (:name, :description, :price, :discount_percentage)");
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':price', $price);
+        $stmt->bindParam(':discount_percentage', $discount_percentage);
+
+        $stmt->execute();
+    }
+    catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+
+    $dbh = null;
+
+    echo ("<script>location.href='$yourURL'</script>");
+ ?>
