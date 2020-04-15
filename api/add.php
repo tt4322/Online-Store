ab6246ed8f58d5bfc0f6ca46c27cfd072ec4d3ae
T <t@Ts-MacBook-Pro.local>
2020-04-15 01:43:03 -0400

    Modifications

--- api/add.php
+++ api/add.php
@@ -1,32 +1,30 @@
 <?php
     $yourURL = "./addRemove.php";
-    $dbhost = "***REMOVED***";
-    $dbname = "***REMOVED***";
-    $username = "***REMOVED***";
-    $password = "***REMOVED***";
+
+    include "../autoload.php";
 
     try {
-        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
-
-        $price = $_POST['price'];
-        $description = $_POST['description'];
-        $name = $_POST['product_name'];
-        $discount_percentage = $_POST['discount_percentage'];
-        $max_discount_percentage = $_POST['max_discount_percentage'];
-
-        $stmt = $dbh->prepare("INSERT INTO products (name, description, price, discount_percentage, max_discount_percentage) VALUES (:name, :description, :price, :discount_percentage, :max_discount_percentage)");
-
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':description', $description);
-        $stmt->bindParam(':price', $price);
-        $stmt->bindParam(':discount_percentage', $discount_percentage);
-        $stmt->bindParam(':max_discount_percentage', $max_discount_percentage);
-
-        $stmt->execute();
+        $dbh = new PDO('mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME'), env('DB_USERNAME'), env('DB_PASSWORD'));
     }
     catch (PDOException $e) {
         echo $e->getMessage();
     }
+
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    $name = $_POST['product_name'];
+    $discount_percentage = $_POST['discount_percentage'];
+    $max_discount_percentage = $_POST['max_discount_percentage'];
+
+    $stmt = $dbh->prepare("INSERT INTO products (name, description, price, discount_percentage, max_discount_percentage) VALUES (:name, :description, :price, :discount_percentage, :max_discount_percentage)");
+
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':description', $description);
+    $stmt->bindParam(':price', $price);
+    $stmt->bindParam(':discount_percentage', $discount_percentage);
+    $stmt->bindParam(':max_discount_percentage', $max_discount_percentage);
+
+    $stmt->execute();
 
     $dbh = null;
 
--- api/remove.php
+++ api/remove.php
@@ -1,12 +1,10 @@
 <?php
     $yourURL = "./addRemove.php";
-    $dbhost = "***REMOVED***";
-    $dbname = "***REMOVED***";
-    $username = "***REMOVED***";
-    $password = "***REMOVED***";
+
+    include "../autoload.php";
 
     try {
-        $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $username, $password);
+        $dbh = new PDO('mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME'), env('DB_USERNAME'), env('DB_PASSWORD'));
     }
     catch (PDOException $e) {
         echo $e->getMessage();
